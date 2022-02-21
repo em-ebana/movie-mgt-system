6b3dea3fcb418c47539e37f6fc73dab19a7f8213
em-ebana <82050121+em-ebana@users.noreply.github.com>
2022-02-21 14:27:46 +0100

    user setting update

--- queries.js
+++ queries.js
@@ -8,9 +8,9 @@ const router = express.Router();
 const app = express();
 const pool = new Pool({
     host: 'localhost',
-   user: 'postgres',
+   user: '******',
    database: 'VMSystem',
-   password: 'password',
+   password: '*******',
    max: 20,
    idleTimeoutMillis: 30000,
    connectionTimeoutMillis: 2000,
